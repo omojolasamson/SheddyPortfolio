adb30b311622cd0488104f3d379c78ef0da3fb63
omojolasamson <omojolasamsonade@gmail.com>
2020-05-29 17:08:06 +0100

    Updates

---
+++ precache-manifest.20a777e07ca05c8528b3db32f5e37b08.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2744e78e0edb95bbe6c8d6ed3ce93a1f",
+    "url": "/sheddyportfolio/index.html"
+  },
+  {
+    "revision": "d5f81acc7e51cd411bb1",
+    "url": "/sheddyportfolio/static/css/2.97dd7354.chunk.css"
+  },
+  {
+    "revision": "e8dd01a68e5af9380c52",
+    "url": "/sheddyportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5f81acc7e51cd411bb1",
+    "url": "/sheddyportfolio/static/js/2.003d8e41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sheddyportfolio/static/js/2.003d8e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8dd01a68e5af9380c52",
+    "url": "/sheddyportfolio/static/js/main.a18dfea0.chunk.js"
+  },
+  {
+    "revision": "ca3a27201e09bcb19b7f",
+    "url": "/sheddyportfolio/static/js/runtime-main.1cbf430d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sheddyportfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sheddyportfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sheddyportfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sheddyportfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sheddyportfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "35ed6b487df43786a71f76daa0d543f5",
+    "url": "/sheddyportfolio/static/media/mypic.35ed6b48.png"
+  }
+]);
